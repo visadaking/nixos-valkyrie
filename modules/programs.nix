7228d8d73829db9c73e0774a22054ad24d2b7777
visa <mauniyahalom@proton.me>
2026-09-03 23:30:03 +0000

    Add modules/programs.nix

---
+++ modules/programs.nix
@@ -0,0 +1,21 @@
+{ ... }:
+
+{
+  programs.steam = {
+    enable = true;
+  };
+
+  programs.gamemode.enable = true;
+  programs.zsh.enable = true;
+
+  # xbox controller support
+  hardware.xone.enable = true;
+
+  # valkyrie shell shortcuts
+  programs.zsh.shellAliases = {
+    v = "nvim";
+    v-switch = "sudo nixos-rebuild switch --flake ~/nixos-valkyrie#valkyrie";
+    v-edit = "nvim ~/nixos-valkyrie";
+    v-clean = "sudo nix-collect-garbage --delete-older-than 30d";
+  };
+}
