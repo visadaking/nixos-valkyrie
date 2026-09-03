b47b3d0149ca658c36b2fcdc731912ea3e80dff9
visa <mauniyahalom@proton.me>
2026-09-03 23:37:07 +0000

    Update modules/programs.nix

--- modules/programs.nix
+++ modules/programs.nix
@@ -11,11 +11,12 @@
   # xbox controller support
   hardware.xone.enable = true;
 
-  # valkyrie shell shortcuts
+  # v switch automation and editor shortcuts
   programs.zsh.shellAliases = {
     v = "nvim";
     v-switch = "sudo nixos-rebuild switch --flake ~/nixos-valkyrie#valkyrie";
     v-edit = "nvim ~/nixos-valkyrie";
     v-clean = "sudo nix-collect-garbage --delete-older-than 30d";
+    v-backup = "git -C ~/nixos-valkyrie push origin main && git -C ~/nixos-valkyrie push backup main";
   };
 }
