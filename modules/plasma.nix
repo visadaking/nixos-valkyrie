16c46d5c78f2251ac1b9e654f9625c5f5c3a3b8b
visa <mauniyahalom@proton.me>
2026-09-04 12:57:54 +0000

    overhaul for plasma.nix
    an entire overhaul to my kde plasma, this is cutting down on the bloat and just having some features i heavily relay on work!

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -9,10 +9,28 @@
     wayland.enable = true;
   };
 
-  # prefer wayland
+  # wayland
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
+
+  # removing kde bloat
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    elisa
+    gwenview
+    okular
+    ark
+    khelpcenter
+    krdp
+    plasma-keyboard
+    kwin-x11
+  ];
+
+  # kde connect
+  programs.kdeconnect.enable = true;
+
+  # KDE Partition Manager
+  programs.partition-manager.enable = true;
 
   # keyboard layout
   services.xserver.xkb = {
@@ -20,7 +38,7 @@
     options = "grp:alt_shift_toggle";
   };
 
-  # plasma customization
+  # plasma theme
   environment.systemPackages = with pkgs; [
     papirus-icon-theme
     capitaine-cursors
