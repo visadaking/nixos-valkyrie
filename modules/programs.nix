3d6c062fd0b8fe52064279421927dfff355d1283
visa <mauniyahalom@proton.me>
2026-09-04 02:03:14 +0000

    merged zsh from  configuration.nix to programs.nix

--- modules/programs.nix
+++ modules/programs.nix
@@ -1,22 +1,30 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.steam = {
     enable = true;
+
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   programs.gamemode.enable = true;
-  programs.zsh.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableBashCompletion = true;
+
+    # valkyrie shell shortcuts
+    shellAliases = {
+      v = "nvim";
+      v-switch = "sudo nixos-rebuild switch --flake ~/nixos-valkyrie#valkyrie";
+      v-edit = "nvim ~/nixos-valkyrie";
+      v-clean = "sudo nix-collect-garbage --delete-older-than 30d";
+      v-backup = "git -C ~/nixos-valkyrie push origin main && git -C ~/nixos-valkyrie push backup main";
+    };
+  };
 
   # xbox controller support
   hardware.xone.enable = true;
-
-  # valkyrie shell shortcuts
-  programs.zsh.shellAliases = {
-    v = "nvim";
-    v-switch = "sudo nixos-rebuild switch --flake ~/nixos-valkyrie#valkyrie";
-    v-edit = "nvim ~/nixos-valkyrie";
-    v-clean = "sudo nix-collect-garbage --delete-older-than 30d";
-    v-backup = "git -C ~/nixos-valkyrie push origin main && git -C ~/nixos-valkyrie push backup main";
-  };
 }
