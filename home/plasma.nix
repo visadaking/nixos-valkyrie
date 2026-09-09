febe346403ca48869395800bc58e3ab96956d9ce
visadaking <yahalom.mauni@protonmail.com>
2026-09-09 19:33:13 +0300

    fix: wire kde config through home manager

--- home/plasma.nix
+++ home/plasma.nix
@@ -1,22 +1,13 @@
 { ... }:
 
 {
-  # kde appearance
   xdg.configFile."kdeglobals".source = ./kde/kdeglobals;
-  xdg.configFile."plasmarc".source = ./kde/plasmarc;
-
-  # plasma desktop, panels and widgets
-  xdg.configFile."plasmashellrc".source = ./kde/plasmashellrc;
-  xdg.configFile."plasma-org.kde.plasma.desktop-appletsrc".source =
-    ./kde/plasma-org.kde.plasma.desktop-appletsrc;
-
-  # kwin settings and rules
+  xdg.configFile."kglobalshortcutsrc".source = ./kde/kglobalshortcutsrc;
+  xdg.configFile."kwinoutputconfig.json".source = ./kde/kwinoutputconfig.json;
   xdg.configFile."kwinrc".source = ./kde/kwinrc;
   xdg.configFile."kwinrulesrc".source = ./kde/kwinrulesrc;
-  xdg.configFile."kwinoutputconfig.json".source =
-    ./kde/kwinoutputconfig.json;
-
-  # global kde shortcuts
-  xdg.configFile."kglobalshortcutsrc".source =
-    ./kde/kglobalshortcutsrc;
+  xdg.configFile."plasma-org.kde.plasma.desktop-appletsrc".source =
+    ./kde/plasma-org.kde.plasma.desktop-appletsrc;
+  xdg.configFile."plasmarc".source = ./kde/plasmarc;
+  xdg.configFile."plasmashellrc".source = ./kde/plasmashellrc;
 }
