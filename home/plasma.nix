aa3504c817912ef03e6b5e695d9620df38c135e5
visadaking <yahalom.mauni@protonmail.com>
2026-09-09 19:31:47 +0300

    fix: kde plasma once again lole

--- home/plasma.nix
+++ home/plasma.nix
@@ -1,4 +1,22 @@
 { ... }:
 
 {
+  # kde appearance
+  xdg.configFile."kdeglobals".source = ./kde/kdeglobals;
+  xdg.configFile."plasmarc".source = ./kde/plasmarc;
+
+  # plasma desktop, panels and widgets
+  xdg.configFile."plasmashellrc".source = ./kde/plasmashellrc;
+  xdg.configFile."plasma-org.kde.plasma.desktop-appletsrc".source =
+    ./kde/plasma-org.kde.plasma.desktop-appletsrc;
+
+  # kwin settings and rules
+  xdg.configFile."kwinrc".source = ./kde/kwinrc;
+  xdg.configFile."kwinrulesrc".source = ./kde/kwinrulesrc;
+  xdg.configFile."kwinoutputconfig.json".source =
+    ./kde/kwinoutputconfig.json;
+
+  # global kde shortcuts
+  xdg.configFile."kglobalshortcutsrc".source =
+    ./kde/kglobalshortcutsrc;
 }
